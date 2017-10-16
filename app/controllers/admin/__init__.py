b670deb7453f28ffa6fac539158bf03ff4ee7fc2
hexinze <hexinze@ajmide.com>
2017-10-16 16:16:21 +0800

    add static file admin

---
+++ app/controllers/admin/__init__.py
@@ -0,0 +1,30 @@
+from flask_admin.contrib.fileadmin import FileAdmin
+from flask_admin.contrib.sqla import ModelView
+
+from flask_login import login_required, current_user
+
+from extensions import admin_permission
+
+
+class CustomModelView(ModelView):
+    """View function of Flask-Admin for Models page."""
+
+    def is_accessible(self):
+        """Setup the access permission for CustomModelView."""
+
+        # callable function `User.is_authenticated()`.
+        # FIXME(JMilkFan):
+        #     Using function is_authenticated(),
+        #     Can return the value of current_user.is_authenticated()
+        #     when user was logged in.
+        return current_user.is_authenticated and admin_permission.can()
+
+
+class CustomFileAdmin(FileAdmin):
+    """File System admin."""
+
+    def is_accessible(self):
+        """Setup the access permission for CustomFileAdmin."""
+
+        # callable function `User.is_authenticated()`.
+        return current_user.is_authenticated and admin_permission.can()
--- run.py
+++ run.py
@@ -1,11 +1,13 @@
+import os
+
 from flask_admin import Admin
+from flask_admin.contrib.fileadmin import FileAdmin
 
 from admin_helper.adminhelper import MyView
-
-from db import session_roles
-
-from roles import User, Role
 from app import create_app
+from app.controllers.admin import CustomFileAdmin
+from db import session_roles
+from roles import User, Role
 
 app = create_app('config')
 
@@ -19,6 +21,10 @@ admin = Admin(app, name='Tse')
 admin.add_view(MyView(name='Hello'))
 admin.add_view(ModelView(User, session_roles))
 admin.add_view(ModelView(Role, session_roles))
-
+basedir = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(basedir, 'app', 'static')
+admin.add_view(CustomFileAdmin(path,
+                               '/static',
+                               name='Static Files'))
 if __name__ == '__main__':
     app.run(debug=True)
