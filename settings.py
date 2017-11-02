4f252ac8380436bdf7063cba1cbd30145722f0f8
hexinze <hexinze@ajmide.com>
2017-11-02 16:13:36 +0800

    remove useless code

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # DB_URI = 'sqlite:///./main.db'
-DB_URI = 'sqlite:///dbs/main.db'
-DB_ROLES_URI = 'sqlite:///dbs/roles.db'
+# DB_URI = 'sqlite:///dbs/main.db'
+# DB_ROLES_URI = 'sqlite:///dbs/roles.db'
 DB_ROLES_URI_AJ = 'sqlite:///dbs/ajstatic.db'
