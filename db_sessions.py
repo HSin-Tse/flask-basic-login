d4ddbc918b286060f5eb1482b116f5abc1157150
hexinze <hexinze@ajmide.com>
2017-11-02 16:12:52 +0800

    remove useless code

--- db_sessions.py
+++ db_sessions.py
@@ -2,12 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
-from settings import DB_URI, DB_ROLES_URI, DB_ROLES_URI_AJ
-
-session = scoped_session(sessionmaker(autocommit=False,
-                                      autoflush=False,
-                                      bind=create_engine(DB_URI)))
-
+from settings import DB_ROLES_URI_AJ
 
 session_roles_aj = scoped_session(sessionmaker(autocommit=False,
                                                autoflush=False,
